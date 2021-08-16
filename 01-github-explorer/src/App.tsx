7ce697a2c7b00e9c73fde727e845b6aee6fb3d41
Gilson Gama <you@example.com>
2021-08-16 02:23:38 -0300

    first commit

---
+++ 01-github-explorer/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { RepositoryList } from './components/RepositoryList';
+import './styles/global.scss';
+
+
+function App() {
+  return (
+    <RepositoryList />
+  );
+}
+
+export default App;
